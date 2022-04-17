6bea6d1900b66255178c153b89e7d488f63aaa24
Eneroice <eneroice@gmail.com>
2022-04-17 03:51:02 +0300

    create main page layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,21 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: ['./app/**/*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'sans': ['Circe', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        'main': '#4188C8',
+        'alt': '#3D3D3D',
+        'gray-100': '#F8F9FB',
+        'gray-200': '#F1F2F4',
+        'green': '#41C877',
+        'red': '#C84141'
+      }
+    },
   },
   plugins: [],
 }
